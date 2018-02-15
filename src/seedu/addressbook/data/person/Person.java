4c4c479999b6290d672ff6f64ba8c0e2afa1b68c
Lim Zheng Kai <limzk1994@hotmail.com>
2018-02-15 11:56:46 +0800

    Added Sequence number and change parser into class level method by adding static

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -14,8 +14,10 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
+    private int SequenceNumber;
 
     private final UniqueTagList tags;
+    private static int count=1;
     /**
      * Assumption: Every field must be present and not null.
      */
@@ -25,6 +27,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.SequenceNumber=count;
+        count++;
     }
 
     /**
